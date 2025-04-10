5f014412529c1f719fcad4f566d146033632a1f5
leehyukshuai <leehyukshuai@stu.pku.edu.cn>
2025-04-10 20:07:25 +0800

    实现了index页面的部分SQL语句

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ def get_db_connection():
 @app.route("/")
 def index():
     # 用户已登录，显示主页内容
-    if 'user' in session:
+    if 'userid' in session:
         # TODO: 获取用户主页内容，包括关注的公众号，最新推文等
-        
+        connection = get_db_connection()
         # 获取参数用于网页显示，默认显示关注的公众号
         try:
             view = request.args.get('view')
@@ -34,17 +34,47 @@ def index():
         
         
         # 注：需要为每行查询结果添加一个 follower_cnt 字段，表示关注人数
+        # TODO: 设置每一个channel的followed属性
         channels = []
         if view == 'followed':
-            # TODO: 将关注的公众号的数据存入 channels 中
-            channels = []
+            # 将关注的公众号的数据存入 channels 中
+            with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+                cursor.execute("""
+                    WITH followed_channels AS (SELECT s.nid FROM subscribe AS s WHERE s.uid = %s),
+                    channel_follower_cnt AS (SELECT s.nid, COUNT(s.uid) AS count FROM subscribe AS s GROUP BY s.nid)
+                    SELECT c.name, u.username AS created_by, cn.count AS follower_cnt, 1 AS followed
+                    FROM followed_channels AS fc
+                    JOIN channels AS c ON fc.nid = c.nid
+                    JOIN users AS u ON u.uid = c.created_by
+                    JOIN channel_follower_cnt AS cn ON cn.nid = c.nid;
+                """, (session['userid']))
+                channels = cursor.fetchall()
         elif view == 'all':
-            # TODO: 将全部公众号的数据存入 channels 中
-            channels = []
+            # 将全部公众号的数据存入 channels 中
+            with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+                cursor.execute("""
+                    WITH followed_channels AS (SELECT s.nid FROM subscribe AS s WHERE s.uid = %s),
+                    channel_follower_cnt AS (SELECT s.nid, COUNT(s.uid) AS count FROM subscribe AS s GROUP BY s.nid)
+                    SELECT c.name, u.username AS created_by, cn.count AS follower_cnt, CASE WHEN c.nid IN (SELECT nid FROM followed_channels) THEN 1 ELSE 0 END AS followed
+                    FROM channels AS c
+                    JOIN users AS u ON u.uid = c.created_by
+                    JOIN channel_follower_cnt AS cn ON cn.nid = c.nid;
+                """, (session['userid']))
+                channels = cursor.fetchall()
         elif view == 'managed':
-            # TODO: 将用户管理的公众号的数据存入 channels 中
-            channels = []
-
+            # 将用户管理的公众号的数据存入 channels 中
+            with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+                cursor.execute("""
+                    WITH followed_channels AS (SELECT s.nid FROM subscribe AS s WHERE s.uid = %s),
+                    managed_channels AS (SELECT s.nid FROM manage_channel AS s WHERE s.uid = %s),
+                    channel_follower_cnt AS (SELECT s.nid, COUNT(s.uid) AS count FROM subscribe AS s GROUP BY s.nid)
+                    SELECT c.name, u.username AS created_by, cn.count AS follower_cnt, CASE WHEN c.nid IN (SELECT nid FROM followed_channels) THEN 1 ELSE 0 END AS followed
+                    FROM managed_channels AS mc
+                    JOIN channels AS c ON mc.nid = c.nid
+                    JOIN users AS u ON users.uid = c.created_by
+                    JOIN channel_follower_cnt AS cn ON cn.nid = c.nid;
+                """, (session['userid'], session['userid']))
+                channels = cursor.fetchall()
 
         # 获取参数用于选择公众号
         try:
@@ -68,10 +98,11 @@ def index():
             posts = []
 
             
+        connection.close()
         # TODO: index 渲染需要三个数据：用户信息，中侧栏所需公众号信息，右侧栏所需推文信息
         return render_template("index.html", 
-            user={'name': 'test'},
-            channels=[],
+            user={'name': session['username']},
+            channels=channels,
             posts=[],
             view_type=view,
             selected_channel=selected_channel
@@ -91,14 +122,15 @@ def login():
         connection = get_db_connection()
         with connection.cursor() as cursor:
             cursor.execute("""
-                SELECT u.password
+                SELECT u.uid, u.password
                 FROM users AS u
                 WHERE u.username = %s
             """, (username))
             result = cursor.fetchone()
-            if result and result[0] == password:
+            if result and result[1] == password:
                 # 如果验证成功，将用户信息存储在session中
-                session['user'] = username
+                session['userid'] = result[0]
+                session['username'] = username
                 return redirect(url_for('index'))
         connection.close()
         flash("Invalid username or password", "danger")
@@ -134,13 +166,14 @@ def register():
 
 @app.route("/logout")
 def logout():
-    session.pop('user', None)
+    session.pop('userid', None)
+    session.pop('username', None)
     return redirect(url_for('index'))
 
 
 @app.route("/follow/<channel_id>")
 def follow(channel_id):
-    if 'user' in session:
+    if 'userid' in session:
         # TODO: 关注公众号
         
         flash("Followed channel {555}", 'info')
@@ -151,7 +184,7 @@ def follow(channel_id):
 
 @app.route("/unfollow/<channel_id>")
 def unfollow(channel_id):
-    if 'user' in session:
+    if 'userid' in session:
         # TODO: 取消关注公众号
 
         flash("Unfollowed channel {555}", 'info')
@@ -162,7 +195,7 @@ def unfollow(channel_id):
 
 @app.route("/delete_post/<post_id>")
 def delete_post(post_id):
-    if 'user' in session:
+    if 'userid' in session:
         # TODO: 删除推文，可能需要检测权限
         flash("Deleted post {555}", 'info')
         return redirect(url_for('index', view=request.args.get('view'), selected_channel=request.args.get('selected_channel')))
